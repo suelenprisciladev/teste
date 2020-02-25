a4860521da667f71f775c4cdfccf2d3e12d5c44f
Suelen Priscila <suelenpriscila.9@gmail.com>
2020-02-25 17:58:36 -0300

    alterações

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -20,7 +20,7 @@ import VueRouter from 'vue-router';
 import VueAxios from 'vue-axios';
 import axios from 'axios';
 import {routes} from './routes';
-import VueCurrencyFilter from 'vue-currency-filter'
+import VueCurrencyFilter from 'vue-currency-filter';
 
 Vue.use(VueRouter);
 Vue.use(VueAxios, axios);
--- resources/assets/js/components/DepositoConta.vue
+++ resources/assets/js/components/DepositoConta.vue
@@ -39,7 +39,6 @@
     export default {
         data() {
             return {
-                valor: null,
                 conta: {},
                 loading: false
             }
